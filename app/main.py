44602443c8070745c751a5193b3f71be613a0eeb
ayukiri <ayumu.vancouver@gmail.com>
2025-04-09 21:57:26 +0900

    Update 2nd greeting message

--- app/main.py
+++ app/main.py
@@ -4,10 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Flask!"
-
-def greet():
     return "Hello, World!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
